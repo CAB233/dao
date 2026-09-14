ce79db87e6b44469216acd11188cdf95ac6dc101
CAB233 <yidaduizuoye@outlook.com>
2026-09-14 21:48:30 +0800

    chore: Bump version to 0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "win.zuoye.dao"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

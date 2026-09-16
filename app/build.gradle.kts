59f0e4564e9c94382118e8451a33a7aeadab1d28
CAB233 <yidaduizuoye@outlook.com>
2026-09-16 19:41:17 +0800

    build: Release version 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "win.zuoye.dao"
         minSdk = 24
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

cc4591f26106871133ad435cd3ff42c315b6f2da
CAB233 <yidaduizuoye@outlook.com>
2026-09-17 19:34:25 +0800

    build: Release version 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "win.zuoye.dao"
         minSdk = 24
         targetSdk = 37
-        versionCode = 8
-        versionName = "0.1.1"
+        versionCode = 9
+        versionName = "0.1.2"
     }
 
     signingConfigs {
